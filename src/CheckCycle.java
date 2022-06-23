44a2a22013bfba0d6c85ea10e5c955e698dc94b0
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-22 20:59:45 -0700

    added annotation

--- src/CheckCycle.java
+++ src/CheckCycle.java
@@ -16,14 +16,19 @@ public class CheckCycle {
         if(head== null|| head.next== null){
             return false;
         }
+        //two pointers point towards the head
         Node slow = head;
         Node fast = head;
+        //loop until the fast reaches to the end
         while(fast!=null && fast.next!=null){
+            //until then move fast by two points
             fast= fast.next.next;
+            //slow by one point
                 slow = slow.next;
+                //where they match return true
                 if(slow == fast){
                     return true;
-            }
+            }//otherwise false
         }return false;
     }
     public static void main(String[] args) {
