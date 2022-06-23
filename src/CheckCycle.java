a0b350fe28aa7e058c70fdb040f52b465dbc3f6e
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-22 20:58:08 -0700

    created a node class and boolean method to check the cycle in the linked list

--- src/CheckCycle.java
+++ src/CheckCycle.java
@@ -1,5 +1,31 @@
 public class CheckCycle {
-    pu
+
+    //Node
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+
+
+    public static boolean checkCycle(Node head){
+        if(head== null|| head.next== null){
+            return false;
+        }
+        Node slow = head;
+        Node fast = head;
+        while(fast!=null && fast.next!=null){
+            fast= fast.next.next;
+                slow = slow.next;
+                if(slow == fast){
+                    return true;
+            }
+        }return false;
+    }
     public static void main(String[] args) {
 
     }
